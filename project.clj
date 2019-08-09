ebdd97c6b397b07cb669fe2cb8f17d6199b4defd
u4bi <rootcode10@gmail.com>
2019-08-09 17:43:22 +0900

    chore: Remove org.clojure/data.json.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clojure-api-starter "0.1.0-SNAPSHOT"
   :main clojure-api-starter.core
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/data.json "0.2.6"]
                  [http-kit "2.3.0"]
                  [compojure "1.6.1"]
                  [ring "1.7.1"]
