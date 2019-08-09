5ff8d6bdc3cd08122f71271ecddfbb60549cf857
u4bi <rootcode10@gmail.com>
2019-08-09 17:03:06 +0900

    chore: Add ring-cors.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [compojure "1.6.1"]
                  [ring "1.7.1"]
                  [ring/ring-json "0.5.0"]
+                 [ring-cors "0.1.13"]
                  [toucan "1.12.0"]
                  [org.postgresql/postgresql "42.2.5"]]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
