8dddee9407b0e8a50ad397e66b831d8dafb1ef67
u4bi <rootcode10@gmail.com>
2019-08-09 16:47:07 +0900

    chore: Add toucan, org.postgresql/postgresql.

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
                  [http-kit "2.3.0"]
                  [compojure "1.6.1"]
                  [ring "1.7.1"]
-                 [ring/ring-json "0.5.0"]]
+                 [ring/ring-json "0.5.0"]
+                 [toucan "1.12.0"]
+                 [org.postgresql/postgresql "42.2.5"]]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
