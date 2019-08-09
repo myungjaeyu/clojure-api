5080ab91228210013fb1303fe82ff2577f330a96
u4bi <rootcode10@gmail.com>
2019-08-09 13:55:57 +0900

    chore: Add initial lein file.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clojure-api-starter "0.1.0-SNAPSHOT"
+  :main clojure-api-starter.core
+  :dependencies [[org.clojure/clojure "1.10.0"]])
---
+++ src/clojure_api_starter/core.clj
@@ -0,0 +1,5 @@
+(ns clojure-api-starter.core
+  (:gen-class))
+
+(defn -main []
+  (println "clojure-api-starter"))
