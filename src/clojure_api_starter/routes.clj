3ba7a2c8b750c1fb2aa25176a4597af2d7335637
u4bi <rootcode10@gmail.com>
2019-08-09 16:56:57 +0900

    feat: Add more orm functions.

--- src/clojure_api_starter/routes.clj
+++ src/clojure_api_starter/routes.clj
@@ -1,16 +1,18 @@
 (ns clojure-api-starter.routes
   (:require [compojure.core :refer [defroutes GET POST PUT DELETE ANY context]]
             [compojure.route :refer [not-found]]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [response]]
+            [clojure-api-starter.service.user :refer [gets-user get-user create-user]]))
 
 (defroutes routes
   (GET "/" []
     (response {:message "index"}))
-  (POST "/" [id name authentication]
-    (response
-     {:id id
-      :name name}))
-  (context "/user/:id" [id]
+  (GET "/users" []
+    (response (gets-user)))
+  (POST "/users" [name]
+    (create-user name)
+    (response {:name name}))
+  (context "/users/:id" [id]
     (GET "/" []
-      (response {:id id})))
+      (response (get-user id))))
   (not-found (response {:message "not found"})))
