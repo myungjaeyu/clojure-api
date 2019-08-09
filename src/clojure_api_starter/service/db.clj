9e84322dad1195bfcbdc473a80d076c92d54b45a
u4bi <rootcode10@gmail.com>
2019-08-09 19:22:54 +0900

    feat: Add env.

--- src/clojure_api_starter/service/db.clj
+++ src/clojure_api_starter/service/db.clj
@@ -1,13 +1,14 @@
 (ns clojure-api-starter.service.db
   (:require [toucan.db :as db]
-            [toucan.models :as models]))
+            [toucan.models :as models]
+            [environ.core :refer [env]]))
 
 (defn db-conn []
-  (db/set-default-db-connection! {:dbtype "postgres"
-                                  :dbname "postgres"
-                                  :user "postgres"
-                                  :host "localhost"
-                                  :password "postgres"}))
+  (db/set-default-db-connection! {:dbtype (env :db-type)
+                                  :dbname (env :db-name)
+                                  :user (env :db-user)
+                                  :host (env :db-host)
+                                  :password (env :db-password)}))
 
 (defn db-root-namespace []
   (models/set-root-namespace! 'clojure-api-starter.models))
